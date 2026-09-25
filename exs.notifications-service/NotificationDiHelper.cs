38f76ca8796627deecb15621800a179dfb9b71f4
Alexander Kesarev <kav.exitonsoftware@gmail.com>
2026-09-25 21:00:52 +0300

    add di helper

---
+++ exs.notifications-service/NotificationDiHelper.cs
@@ -0,0 +1,17 @@
+﻿using exs.notifications_service.Impl;
+using exs.notifications_service.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace exs.notifications_service
+{
+	public static class NotificationDiHelper
+	{
+		public static IServiceCollection AddNotificationSender(this IServiceCollection services)
+		{
+			return services
+				.AddScoped<INotificationSender, NotificationSender>()
+				.AddSingleton<INotificationService, NotificationService>()
+				.AddHostedService(sp => (NotificationService)sp.GetRequiredService<INotificationService>());
+		}
+	}
+}
